13c3b70d0756385daf97306f1057ef701595adcd
Aman Jadon <jadonaman471@gmail.com>
2024-09-29 02:22:30 +0530

    port added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from "./app.js"
 import routes from "./route.js"
 app.use("/api", routes)
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 app.listen(port, () => {
     console.log(`server is listening at ${port}`)
 })
